6480f97fb6aa73df787a374e675ba142b26523f4
RAFEEF <RAFEEF@10.0.0.65>
2025-01-31 18:18:12 -0500

    added more code

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -3,6 +3,13 @@ package day2;
 public class DemoDay2 {
 	public static void main (String args[]) {
 		System.out.println("Rafeef");
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+		
+		String name = "Rafeef";
+		int id = 111;
+		System.out.println(name + id);
 	}
 
 }
